dc231b6b96f0d391156d97c1fea951e3518e166c
yuri-kevin <yuri.kevin@square63.com>
2023-11-03 17:44:34 +0500

    Styled header and banner

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
 
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,14 +12,25 @@ import './Header.scss';
 
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className='py-4'>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <div className="logo">
+          <Navbar.Brand href="#home" className='p-0 d-flex w-100'>
+            <img src={logo} alt="Logo" />
+          </Navbar.Brand>
+        </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="mx-auto center-nav">
+            <Nav.Link href="#home">Demos</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Blog</Nav.Link>
+            <Nav.Link href="#link">Pages</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#login">Login</Nav.Link>
+            <Button variant="primary">Get Started Free</Button>{' '}
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -26,3 +39,4 @@ function Header() {
 }
 
 export default Header
+                                                
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+import AIchatbot from '../../assets/images/banner-img.png';
 import './Home.scss';
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <div className="main-banner">
+        <div className="banner-inner d-flex justify-content-end align-items-center">
+          <div className="banner-text">
+            <h2>
+              Empathetic Mental Health <span>AI Chatbot</span>
+            </h2>
+            <h5>Our AI assistant UVO is here to help you navigate the world of health and wellness.</h5>
+            <div className="bnt-section mt-5">
+              <Button variant="outline-primary me-1">Try Demo</Button>{' '}
+              <Button variant="primary">Get Started</Button>{' '}
+            </div>
+          </div>
+          <div className="AIchatbot-img">
+            <img src={AIchatbot} alt="Logo" />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
