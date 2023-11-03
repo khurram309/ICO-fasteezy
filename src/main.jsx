5b8bd497575aac1ddc8e233b8e07c75186b8c106
yuri-kevin <yuri.kevin@square63.com>
2023-11-03 17:44:33 +0500

    change css to scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Home from './components/Home/Home.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.scss';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Home from './components/Home/Home.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
