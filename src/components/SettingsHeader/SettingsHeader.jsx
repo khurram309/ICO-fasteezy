4b7f1bfd7627bad2de077ae85ccad2500473b356
khurram shoukat <khurramshoukat441@gmail.com>
2023-12-04 16:02:06 +0500

    Added and styled user profile dropdown

--- src/components/SettingsHeader/SettingsHeader.jsx
+++ src/components/SettingsHeader/SettingsHeader.jsx
@@ -10,7 +10,7 @@ function SettingsHeader() {
         <h3 className='m-0'>Hey there, Brian Ford!</h3>
         <p className='gray85'>Welcome back, we're happy to have you here!</p>
 
-        <Navbar bg="transparent" className="bg-transparent border-0 border-bottom rounded-0 p-0">
+        <Navbar bg="transparent" className="bg-transparent border-0 border-bottom rounded-0 p-0 mt-5">
           <Nav className="mr-auto" activeKey="/settings/general">
             <NavLink to="/settings/general">General</NavLink>
             <NavLink to="/settings/billing">Billing</NavLink>
