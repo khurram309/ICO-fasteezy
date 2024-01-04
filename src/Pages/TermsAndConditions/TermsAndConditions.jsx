16a571e8f20501a3339ab7579719c399fc6a6645
khurram shoukat <khurramshoukat441@gmail.com>
2024-01-04 17:15:32 +0500

    Styled chatbot modal

--- src/Pages/TermsAndConditions/TermsAndConditions.jsx
+++ src/Pages/TermsAndConditions/TermsAndConditions.jsx
@@ -1,8 +1,89 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 function TermsAndConditions() {
   return (
-    <div>TermsAndConditions</div>
+    <Container fluid="lg" className='policy-pages'>
+      <h4 className='mt-5 mb-4'>Terms of Service for UVO AI - AI Doctor Advice Application</h4>
+
+    <ol>
+      <li>
+        Introduction
+        <div>
+          Welcome to UVO AI, your virtual health assistant. By using UVO AI, you agree to comply with and be bound by the following terms and conditions
+        </div>
+      </li>
+      <li>
+        Use of UVO AI
+        <ol>
+          <li>
+            UVO AI is an AI-powered application designed to provide general health advice based on the information provided by users. It is not a substitute for professional medical advice, diagnosis, or treatment.
+          </li>
+          <li>
+            Users should not disregard professional medical advice or delay seeking it based on information received from UVO AI.
+          </li>
+        </ol>
+      </li>
+      <li>
+          User Eligibility
+        <div>
+          By using UVO AI, you affirm that you are at least 18 years old or have obtained parental consent.
+        </div>
+      </li>
+      <li>
+        Medical Disclaimer
+        <ol>
+          <li>
+            UVO AI is not a licensed medical professional. The advice provided is based on algorithms and user-provided information and should not be considered as a substitute for professional medical judgment.
+          </li>
+          <li>
+            Users are advised to consult with a qualified healthcare professional for accurate diagnosis and treatment.
+          </li>
+        </ol>
+      </li>
+      <li>
+        User Information
+        <div>
+          Users are responsible for providing accurate and up-to-date information for the AI to generate relevant advice. UVO AI is not liable for any consequences resulting from inaccurate or incomplete user-provided data.
+        </div>
+      </li>
+      <li>
+        Limitation of Liability
+        <ol>
+          <li>
+            UVO AI and its creators shall not be liable for any direct, indirect, incidental, consequential, or special damages arising from the use or inability to use the application.
+          </li>
+          <li>
+            UVO AI does not guarantee the accuracy, completeness, or usefulness of the information provided.
+          </li>
+        </ol>
+      </li>
+      <li>
+        Privacy
+        <div>
+          UVO AI is committed to protecting user privacy. Please refer to our Privacy Policy for details on how user data is collected, used, and protected.
+        </div>
+      </li>
+      <li>
+        Changes to Terms
+        <div>
+          UVO AI reserves the right to modify these terms at any time. Users will be notified of any changes, and continued use of the application implies acceptance of the modified terms.
+        </div>
+      </li>
+      <li>
+        Governing Law
+        <div>
+          These terms are governed by and construed in accordance with the laws of USA
+        </div>
+      </li>
+      <li>
+        Contact Information
+        <div>
+          For questions or concerns regarding these terms, please contact <a href="mailto:admin@uvohealth.com">admin@uvohealth.com</a>.
+        </div>
+      </li>
+    </ol>
+    </Container>
   )
 }
 
--- src/components/Modals/PremiumPlan/PremiumPlan.jsx
+++ src/components/Modals/PremiumPlan/PremiumPlan.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Modal, Form } from 'react-bootstrap';
+import { Modal, Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from 'react-bootstrap';
 
 import './PremiumPlan.scss';
 
@@ -17,13 +17,33 @@ function PremiumPlan(props) {
         keyboard={false}
         centered className="users-modal"
       >
-        <Modal.Header closeButton className="d-flex flex-column-reverse pb-0">
-          <Modal.Title>You have exceeded the free usage. Upgrade to have unlimited acces</Modal.Title>
-        </Modal.Header>
+        
         <Modal.Body>
-          <Button className="w-100" variant="primary">
-            Upgrade to Premiun
-          </Button>
+          <Modal.Header closeButton className='p-0 mb-3'>
+          </Modal.Header>
+          <Card className="h-100">
+              <CardBody className="p-0">
+                <div className="d-flex flex-column justify-content-between h-100">
+                  <div>
+                    <CardTitle>$4.99<span>/month</span></CardTitle>
+                    <CardSubtitle className="py-2">Premium Plan</CardSubtitle>
+                    <CardText className="pb-3">Upgrade to our Premium Plan for just $4.99 per month and enjoy unlimited access to our medical chat bot. Get answers to all your health questions, anytime you need them.</CardText>
+                    <hr />
+                    <div className="list-view">
+                      <ul className="mt-3">
+                        <li>Limited access to the medical chat bot.</li>
+                        <li>Ask as many questions as you want.</li>
+                        <li>Priority support for faster responses.</li>
+                        <li>Cancel anytime.</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <Button className="w-100 mt-3">
+                    Upgrade to Premium
+                  </Button>
+                </div>
+              </CardBody>
+            </Card>
         </Modal.Body>
       </Modal>
     </>
